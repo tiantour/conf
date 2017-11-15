ce17e9be5ca8328df3196162aad8c96c2db94ada
姜灏 <150haomengting@163.com>
2017-11-15 18:59:11 +0800

    upgrade conf

--- conf.go
+++ conf.go
@@ -1,162 +1,102 @@
 package conf
 
 import (
-	"flag"
-	"fmt"
 	"log"
-	"os"
 
 	"github.com/BurntSushi/toml"
 )
 
-var (
-	conf    = flag.String("conf", "", "set config file name")
-	newConf = new(Conf)
-)
+// Path toml path
+var Path = "conf.toml"
 
-type (
-	// Conf conf
-	Conf struct {
-		Server   server   `toml:"server"`
-		DB       db       `toml:"db"`
-		Cache    cache    `toml:"cache"`
-		Account  account  `toml:"account"`
-		Upyun    upyun    `toml:"upyun"`
-		Qiniu    qiniu    `toml:"qiniu"`
-		Weather  weather  `toml:"weather"`
-		Wechat   wechat   `toml:"wechat"`
-		Weibo    weibo    `toml:"weibo"`
-		QQ       qq       `toml:"qq"`
-		MP       mp       `toml:"mp"`
-		Wxpay    wxpay    `toml:"wxpay"`
-		Mpay     mpay     `toml:"mpay"`
-		Alipay   alipay   `toml:"alipay"`
-		Alidayu  alidayu  `toml:"alidayu"`
-		Template template `toml:"template"`
-	}
-	// server
-	server struct {
-		Domain string
-		Host   string
-		Port   string
-		Upload string
-		Start  string
-	}
-	// DB db
-	db struct {
-		Uname    string
-		Passwd   string
-		Host     string
-		Port     string
-		Database string
-	}
-	// cache
-	cache struct {
-		Host string
-		Port string
-	}
-	// account
-	account struct {
-		Default int
-		Secret  string
-		Issuer  string
-		Avatar  string
-		Gender  string
-		Salt    string
-	}
-	// upaiyun
-	upyun struct {
-		Bucket   string
-		Username string
-		Passwd   string
-		Host     string
-	}
-	// qiniu
-	qiniu struct {
-		AccessKey string
-		SecretKey string
-		Bucket    string
-		Host      string
-	}
-	// weather
-	weather struct {
-		ID  string
-		Key string
-	}
-	// wechat
-	wechat struct {
-		AppID        string
-		AppSecret    string
-		WebAppID     string
-		WebAppSecret string
-		Token        string
-	}
-	// weibo
-	weibo struct {
-		AppID     string
-		AppSecret string
-	}
-	// qq
-	qq struct {
-		AppID     string
-		AppSecret string
-	}
-	mp struct {
-		AppID     string
-		AppSecret string
-	}
-	// wxpay
-	wxpay struct {
-		MchID string
-		Key   string
-	}
-	// mpay
-	mpay struct {
-		MchID string
-		Key   string
-	}
-	// alipay
-	alipay struct {
-		AppID      string
-		PublicKey  string
-		PrivateKey string
-	}
-	// alidayu
-	alidayu struct {
-		AppKey    string
-		AppSecret string
-		Template  string
-		Sign      string
-	}
-	template struct {
-		Feedback string
-		Stock    string
-		Notice   string
-		Warn     string
-		Material string
-		Report   string
-	}
-)
+// Conf conf
+type Conf struct {
+	Server  map[string]server
+	DB      db
+	Cache   cache
+	Token   token
+	UpYun   upyun
+	QiNiu   qiniu
+	Wechat  map[string]wechat
+	Weibo   weibo
+	QQ      qq
+	Alipay  alipay
+	Wxpay   map[string]wxpay
+	Dayu    map[string]dayu
+	Free    map[string]service
+	Manager map[string]service
+	User    map[string]service
+}
+type server struct {
+	IP   string
+	Port string
+	Host string
+	Env  string
+}
+type db struct {
+	IP     string
+	Port   string
+	DB     string
+	Uname  string
+	Passwd string
+}
+type cache struct {
+	IP   string
+	Port string
+}
+type token struct {
+	Secret string
+	Issuer string
+}
+type qiniu struct {
+	Bucket    string
+	Host      string
+	AccessKey string `toml:"access_key"`
+	SecretKey string `toml:"secret_key"`
+}
+type upyun struct {
+	Bucket string
+	Host   string
+	Uname  string
+	Passwd string
+}
+type wechat struct {
+	AppID     string `toml:"app_id"`
+	AppSecret string `toml:"app_secret"`
+}
+type weibo struct {
+	AppID     string `toml:"app_id"`
+	AppSecret string `toml:"app_secret"`
+}
+type qq struct {
+	AppID  string `toml:"app_id"`
+	AppKey string `toml:"app_key"`
+}
+type alipay struct {
+	AppID      string `toml:"app_id"`
+	PublicKey  string `toml:"public_key"`
+	PrivateKey string `toml:"private_key"`
+}
+type wxpay struct {
+	MchID string `toml:"mch_id"`
+	Key   string `toml:"key"`
+}
+type dayu struct {
+	AppKey    string `toml:"app_key"`
+	AppSecret string `toml:"app_secret"`
+	Sign      string
+}
+type service struct {
+	Name string
+	Port string
+}
 
 // NewConf new conf
 func NewConf() *Conf {
-	return newConf
-}
-func init() {
-	flag.Parse()
-	if *conf == "" {
-		log.Println("conf file is null")
-		flag.Usage()
-		os.Exit(1)
-	}
-	env := os.Getenv("ENV")
-	if env != "" {
-		env = fmt.Sprintf("%s.", env)
-	}
-	path := fmt.Sprintf("../conf/conf.%s.%stoml", *conf, env)
-	_, err := toml.DecodeFile(path, newConf)
+	result := &Conf{}
+	_, err := toml.DecodeFile(Path, result)
 	if err != nil {
-		log.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
+	return result
 }
