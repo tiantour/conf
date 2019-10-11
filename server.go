1994f5f3a20b9bf7402460759f91686be197ebed
andy <andy@andyMacBook-Pro.local>
2019-10-11 17:08:12 +0800

    add snmp

--- server.go
+++ server.go
@@ -11,14 +11,16 @@ func NewServer() *Server {
 }
 
 type server struct {
-	IDC     int32   `toml:"idc"`     // 机房
-	Model   int32   `toml:"model"`   // 型号
-	Name    string  `toml:"name"`    // 备注
-	Address string  `toml:"address"` // 链接
-	Uname   string  `toml:"uname"`   // 用户
-	Passwd  string  `toml:"passwd"`  // 密码
-	Uint    string  `toml:"unit"`    // 单位
-	Min     float64 `toml:"min"`     // 最大
-	Max     float64 `toml:"max"`     // 最小
-	Warning float64 `toml:"warning"` // 报警
+	IDC       int32   `toml:"idc"`       // 机房
+	Model     int32   `toml:"model"`     // 型号
+	Name      string  `toml:"name"`      // 备注
+	Address   string  `toml:"address"`   // 链接
+	Uname     string  `toml:"uname"`     // 用户
+	Passwd    string  `toml:"passwd"`    // 密码
+	Community string  `toml:"community"` // 社区
+	Point     string  `toml:"point"`     // 位置
+	Uint      string  `toml:"unit"`      // 单位
+	Min       float64 `toml:"min"`       // 最大
+	Max       float64 `toml:"max"`       // 最小
+	Warning   float64 `toml:"warning"`   // 报警
 }
