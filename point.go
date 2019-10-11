5f15f14e011b1ed25dc06e3b3a4006503ae33f0d
andy <andy@andyMacBook-Pro.local>
2019-10-11 21:56:42 +0800

    fix bugs

--- point.go
+++ point.go
@@ -12,7 +12,7 @@ func NewPoint() *Point {
 
 type point struct {
 	Name    string  `toml:"name"`    // 备注
-	Address uint16  `toml:"address"` // 地址
+	Address string  `toml:"address"` // 地址
 	Uint    string  `toml:"unit"`    // 单位
 	Min     float64 `toml:"min"`     // 最大
 	Max     float64 `toml:"max"`     // 最小
