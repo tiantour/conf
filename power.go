593cc8b5b21b222a3e75f38a802dbd40290bdc64
andy <andy@andyMacBook-Pro.local>
2019-11-18 10:42:35 +0800

    fix bugs

--- power.go
+++ power.go
@@ -15,15 +15,14 @@ type power struct {
 	Function uint16   `toml:"function"` // 功能
 	IP       string   `toml:"ip"`       // 地址
 	Location uint16   `toml:"location"` // 地址
-	Name     string   `toml:"name"`     // 名字
+	Name     []string `toml:"name"`     // 名字
 	Address  uint16   `toml:"address"`  // 地址
-	Switch   uint16   `toml:"switch"`   // 开关
 	Quantity uint16   `toml:"quantity"` // 数量
 	Amount   uint16   `toml:"amount"`   // 数量
 	Reverse  uint16   `toml:"reverse"`  // 反转
 	Signed   uint16   `toml:"signed"`   // 符号
 	Decimal  uint16   `toml:"decimal"`  // 小数
-	Compose  []string `toml:"compose"`  // 批量
+	Switch   uint16   `toml:"switch"`   // 开关
 	Uint     string   `toml:"unit"`     // 单位
 	Min      float64  `toml:"min"`      // 最大
 	Max      float64  `toml:"max"`      // 最小
