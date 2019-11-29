8024007ef1c0d0df7a8ad6fa6b6b0a048d80f06f
andy <andy@andyMacBook-Pro.local>
2019-11-29 16:19:04 +0800

    fix bugs

--- alipay.go
+++ alipay.go
@@ -12,6 +12,7 @@ func NewAlipay() *Alipay {
 
 type alipay struct {
 	AppID      string `toml:"app_id"`      // 编号
+	AesKey     string `toml:"aes_key"`     // AES
 	PublicKey  string `toml:"public_key"`  // 公钥
 	PrivateKey string `toml:"private_key"` // 私钥
 }
