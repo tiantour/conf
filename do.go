e94e949b6a5591e3c3189705eb1c9ca53cd1b9a1
andy <150haomengting@163.com>
2021-11-18 16:33:10 +0800

    fix bugs

--- do.go
+++ do.go
@@ -4,6 +4,7 @@ package conf
 type DO struct {
 	Model  int32  `toml:"model"`  // 型号
 	Device string `toml:"device"` // 设备
+	Name   string `toml:"name"`   // 名称
 	Point  uint8  `toml:"point"`  // 地址
 	Action uint8  `toml:"action"` // 地址
 }
