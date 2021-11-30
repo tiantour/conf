44886ac31f0c2be1341d1341ae5816e5ef109fdd
andy <150haomengting@163.com>
2021-11-30 10:38:48 +0800

    fix bugs

--- command.go
+++ command.go
@@ -11,15 +11,14 @@ func NewCommand() *Command {
 }
 
 type command struct {
-	SlaveID  byte   `toml:"slaveid"`  // 站号
-	Location uint16 `toml:"location"` // 地址
-	Function uint16 `toml:"function"` // 功能
-	Address  uint16 `toml:"address"`  // 地址
-	Quantity uint16 `toml:"quantity"` // 数量
-	Value    []byte `toml:"value"`    // 数值
-	Uname    string `toml:"uname"`    // 用户
-	Passwd   string `toml:"passwd"`   // 密码
-	Code     string `toml:"code"`     // 命令
-	Point    uint8  `toml:"point"`    // 地址
-	State    uint8  `toml:"state"`    // 状态
+	SlaveID  byte   `toml:"slaveid"`  // 站号 modbus
+	Slot     int    `toml:"slot"`     // 插槽 s7
+	Function uint16 `toml:"function"` // 功能 modbus&s7
+	Address  uint16 `toml:"address"`  // 地址 modbus&s7
+	Location uint16 `toml:"location"` // 地址 modbus&s7
+	Quantity uint16 `toml:"quantity"` // 数量 modbus&s7
+	Value    []byte `toml:"value"`    // 数值 modbus&s7
+	Uname    string `toml:"uname"`    // 用户 impi
+	Passwd   string `toml:"passwd"`   // 密码 impi
+	Code     string `toml:"code"`     // 命令 impi
 }
