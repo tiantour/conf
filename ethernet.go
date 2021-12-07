04c07d6185162d911e7d4b34a329ceb0c85206b9
andy <150haomengting@163.com>
2021-12-07 15:15:56 +0800

    fix bugs

--- ethernet.go
+++ ethernet.go
@@ -11,9 +11,9 @@ func NewEthernet() *Ethernet {
 }
 
 type ethernet struct {
-	IP      string `toml:"ip"`      // 地址
-	Rack    int    `toml:"rack"`    // 架子
-	Port    int    `toml:"port"`    // 端口
-	Slot    []int  `toml:"slot"`    // 插槽
-	SlaveID []byte `toml:"slaveid"` // 站号
+	IP      []string `toml:"ip"`      // 地址
+	Rack    int      `toml:"rack"`    // 架子
+	Port    int      `toml:"port"`    // 端口
+	Slot    int      `toml:"slot"`    // 插槽
+	SlaveID byte     `toml:"slaveid"` // 站号
 }
