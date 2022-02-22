991afbba64cb2f9d7f712f5af0f739828e343d8f
andy <150haomengting@163.com>
2022-02-22 23:53:22 +0800

    add wxpay v3

--- wxpay.go
+++ wxpay.go
@@ -11,6 +11,10 @@ func NewWxpay() *Wxpay {
 }
 
 type wxpay struct {
-	MchID string `toml:"mch_id"` // 编号
-	Key   string `toml:"key"`    // 键值
+	MchID      string `toml:"mch_id"`      // 编号
+	Key        string `toml:"key"`         // API密钥
+	Key3       string `toml:"key3"`        // API密钥v3
+	ClientCert string `toml:"client_cert"` // 证书
+	ClientKey  string `toml:"client_key"`  // 私钥
+	SerialNo   string `toml:"serial_no"`   // 证书编号
 }
